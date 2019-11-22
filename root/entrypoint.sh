d5bffbbec35618e22bb0dbae9a9b923c38bcd196
Janus <me@janus.ovh>
2019-11-22 21:05:25 +0100

    Fix entrypoint cron

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -8,35 +8,35 @@ else
 fi
 
 case "$SCAN_INTERVAL" in
-  5m)  echo "*/5 *   *  *  *    /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "*/5 *   *  *  *    /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  5m)  echo "*/5 *   *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+       echo "*/5 *   *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
   ;;
-  10m) echo "*/10 *  *  *  *    /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "*/10 *  *  *  *    /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  10m) echo "*/10 *  *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+       echo "*/10 *  *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
   ;;
-  15m) echo "*/15 *  *  *  *    /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "*/15 *  *  *  *    /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  15m) echo "*/15 *  *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+       echo "*/15 *  *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
   ;;
-  30m) echo "*/30 *  *  *  *    /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "*/30 *  *  *  *    /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  30m) echo "*/30 *  *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+       echo "*/30 *  *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
   ;;
-  1h)  echo "0 *  *  *  *    /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "0 *  *  *  *    /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  1h)  echo "0 *  *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+       echo "0 *  *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
   ;;
-  2h)  echo "0 */2 *  *  *    /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "0 */2 *  *  *    /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  2h)  echo "0 */2 *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+       echo "0 */2 *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
   ;;
-  4h)  echo "0 */4 *  *  *    /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "0 */4 *  *  *    /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  4h)  echo "0 */4 *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+       echo "0 */4 *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
   ;;
-  6h)  echo "0 */6 *  *  *    /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "0 */6 *  *  *    /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  6h)  echo "0 */6 *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+       echo "0 */6 *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
   ;;
-  12h) echo "0 */12 *  *  *    /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "0 */12 *  *  *    /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  12h) echo "0 */12 *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+       echo "0 */12 *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
   ;;
-  *)   echo "0 * *  *  *    /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "0 * *  *  *    /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  *)   echo "0 * *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+       echo "0 * *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
   ;;
 esac
 
