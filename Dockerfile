46a26f995dcdbdadd9e3523db36dbb81856c36b6
Janus <me@janus.ovh>
2019-12-03 13:38:08 +0100

    Minor update structure
    Change from cron service to supervisord (start cron has no daemon)
    Change entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,14 @@ LABEL maintainer="me@janus.ovh"
 ENV DEBIAN_FRONTEND=noninteractive TERM=linux TZ=Europe/Paris
 ENV SCAN_INTERVAL=1h AGENT_KEY='' API_KEY=''
 
-ADD root/entrypoint.sh /entrypoint.sh
 RUN apt update && \
-    apt install -y --no-install-recommends fping git cron php-pear && \
+    apt install -y --no-install-recommends fping git cron php-pear ca-certificates supervisor vim && \
     apt install -y --no-install-recommends libgmp-dev re2c libmhash-dev libmcrypt-dev file && \
     docker-php-ext-install pdo pdo_mysql mbstring iconv ctype gettext gmp json pcntl && \
-    git clone  --recursive https://github.com/AnthoDingo/phpipam-agent.git /phpipam && \
-    touch /var/log/cron.log && \
-    chmod  750 /entrypoint.sh
+    git clone  --recursive https://github.com/AnthoDingo/phpipam-agent.git /phpipam
+
+ADD root /
+RUN  chmod  750 /entrypoint.sh
 
 WORKDIR /phpipam
-CMD /entrypoint.sh
+CMD ['/entrypoint.sh']
--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -1,44 +1,53 @@
 #!/bin/sh
+set -e
 
 if [ -f /usr/share/zoneinfo/$TZ ]; then
-  setup-timezone -z $TZ
+  #setup-timezone -z $TZ
+  echo "$TZ" > /etc/timezone
+  dpkg-reconfigure -f noninteractive tzdata
   echo "[Date]\ndate.timezone = $TZ" >/usr/local/etc/php/conf.d/99_tz.ini
 else
   echo "[Date]\ndate.timezone = UTC" >/usr/local/etc/php/conf.d/99_tz.ini
 fi
 
 case "$SCAN_INTERVAL" in
-  5m)  echo "*/5 *   *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "*/5 *   *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  5m)  echo "*/5 *   *  *  *  php /phpipam/index.php update > /dev/stdout 2>/dev/stderr" >/var/spool/cron/crontabs/root
+       echo "*/5 *   *  *  *  php /phpipam/index.php discover > /dev/stdout 2>/dev/stderr" >>/var/spool/cron/crontabs/root
   ;;
-  10m) echo "*/10 *  *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "*/10 *  *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  10m) echo "*/10 *  *  *  *  php /phpipam/index.php update > /dev/stdout 2>/dev/stderr" >/var/spool/cron/crontabs/root
+       echo "*/10 *  *  *  *  php /phpipam/index.php discover > /dev/stdout 2>/dev/stderr" >>/var/spool/cron/crontabs/root
   ;;
-  15m) echo "*/15 *  *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "*/15 *  *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  15m) echo "*/15 *  *  *  *  php /phpipam/index.php update > /dev/stdout 2>/dev/stderr" >/var/spool/cron/crontabs/root
+       echo "*/15 *  *  *  *  php /phpipam/index.php discover > /dev/stdout 2>/dev/stderr" >>/var/spool/cron/crontabs/root
   ;;
-  30m) echo "*/30 *  *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "*/30 *  *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  30m) echo "*/30 *  *  *  *  php /phpipam/index.php update > /dev/stdout 2>/dev/stderr" >/var/spool/cron/crontabs/root
+       echo "*/30 *  *  *  *  php /phpipam/index.php discover > /dev/stdout 2>/dev/stderr" >>/var/spool/cron/crontabs/root
   ;;
-  1h)  echo "0 *  *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "0 *  *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  1h)  echo "0 *  *  *  *  php /phpipam/index.php update > /dev/stdout 2>/dev/stderr" >/var/spool/cron/crontabs/root
+       echo "0 *  *  *  *  php /phpipam/index.php discover > /dev/stdout 2>/dev/stderr" >>/var/spool/cron/crontabs/root
   ;;
-  2h)  echo "0 */2 *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "0 */2 *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  2h)  echo "0 */2 *  *  *  php /phpipam/index.php update > /dev/stdout 2>/dev/stderr" >/var/spool/cron/crontabs/root
+       echo "0 */2 *  *  *  php /phpipam/index.php discover > /dev/stdout 2>/dev/stderr" >>/var/spool/cron/crontabs/root
   ;;
-  4h)  echo "0 */4 *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "0 */4 *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  4h)  echo "0 */4 *  *  *  php /phpipam/index.php update > /dev/stdout 2>/dev/stderr" >/var/spool/cron/crontabs/root
+       echo "0 */4 *  *  *  php /phpipam/index.php discover > /dev/stdout 2>/dev/stderr" >>/var/spool/cron/crontabs/root
   ;;
-  6h)  echo "0 */6 *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "0 */6 *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  6h)  echo "0 */6 *  *  *  php /phpipam/index.php update > /dev/stdout 2>/dev/stderr" >/var/spool/cron/crontabs/root
+       echo "0 */6 *  *  *  php /phpipam/index.php discover > /dev/stdout 2>/dev/stderr" >>/var/spool/cron/crontabs/root
   ;;
-  12h) echo "0 */12 *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "0 */12 *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  12h) echo "0 */12 *  *  *  php /phpipam/index.php update > /dev/stdout 2>/dev/stderr" >/var/spool/cron/crontabs/root
+       echo "0 */12 *  *  *  php /phpipam/index.php discover > /dev/stdout 2>/dev/stderr" >>/var/spool/cron/crontabs/root
   ;;
-  *)   echo "0 * *  *  *  php /phpipam/index.php update >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
-       echo "0 * *  *  *  php /phpipam/index.php discover >> /var/log/cron.log 2>&1" >>/etc/cron.d/phpipam
+  *)   echo "0 * *  *  *  php /phpipam/index.php update > /dev/stdout 2>/dev/stderr" >/var/spool/cron/crontabs/root
+       echo "0 * *  *  *  php /phpipam/index.php discover > /dev/stdout 2>/dev/stderr" >>/var/spool/cron/crontabs/root
   ;;
 esac
 
-cron
-tail -f /var/log/cron.log
+chmod 644 /var/spool/cron/crontabs/root
+
+# Run one time on Container start
+php /phpipam/index.php update
+php /phpipam/index.php discover
+
+# Start supervisord
+/usr/bin/supervisord
